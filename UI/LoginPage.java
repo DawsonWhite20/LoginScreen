a928a0731ffdcc045c670aeed7402a91b067fa13
dwhit <dwhite005@icloud.com>
2024-03-08 14:18:02 -0500

    Started learning Swing for Java UI

---
+++ UI/LoginPage.java
@@ -0,0 +1,16 @@
+package UI;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class LoginPage {
+
+    public LoginPage () {
+        JFrame frame = new JFrame();
+        frame.setSize(100, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+    }
+}
--- main.java
+++ main.java
@@ -1,5 +1,9 @@
+import UI.LoginPage;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("Test");
+
+        LoginPage loginPage = new LoginPage();
     }
 }
