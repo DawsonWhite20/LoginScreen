2af6f9e31ce53e3f6b81ddc466431d2a6a8735ad
dwhit <dwhite005@icloud.com>
2024-03-12 17:14:05 -0400

    Creating the panel in login screen
    Panel is like div in html

--- UI/LoginPage.java
+++ UI/LoginPage.java
@@ -6,14 +6,24 @@ import javax.swing.JPanel;
 
 public class LoginPage {
 
+    void test() {
+        System.out.println("Hello");
+    }
+
     public LoginPage () {
         JFrame frame = new JFrame();
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setBackground(Color.decode("#ded9ee"));
         frame.pack();
         frame.setVisible(true);
-
+        
         JPanel panel = new JPanel();
+        frame.add(panel);
+
+        composingPanel(panel);
+    }
+
+    private void composingPanel(JPanel panel) {
+        panel.setBackground(Color.decode("#ded9ee"));
     }
 }
