5fe8af9b570835e2f3a2b6b50c6c833dfb38d108
dwhit <dwhite005@icloud.com>
2024-03-12 21:03:30 -0400

    Added Jlabel for username

--- UI/LoginPage.java
+++ UI/LoginPage.java
@@ -1,14 +1,15 @@
 package UI;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class LoginPage {
 
-    void test() {
-        System.out.println("Hello");
-    }
 
     public LoginPage () {
         JFrame frame = new JFrame();
@@ -17,13 +18,24 @@ public class LoginPage {
         frame.pack();
         frame.setVisible(true);
         
-        JPanel panel = new JPanel();
+        JPanel panel = new JPanel(new GridBagLayout());
         frame.add(panel);
 
         composingPanel(panel);
     }
 
     private void composingPanel(JPanel panel) {
+        JLabel libraryName = new JLabel("Gray Java Library");
+        libraryName.setBounds(1200, 450, 300, 100);
+        libraryName.setFont(new Font("Arial", Font.BOLD, 25));
+        JLabel username = new JLabel("Username");
+        username.setBounds(1250, 500, 150, 100);
+        username.setFont(new Font("serif", Font.LAYOUT_LEFT_TO_RIGHT, 15));
+
+
+        panel.setLayout(null);
+        panel.add(libraryName);
+        panel.add(username);
         panel.setBackground(Color.decode("#ded9ee"));
     }
 }
--- main.java
+++ main.java
@@ -2,7 +2,6 @@ import UI.LoginPage;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Test");
 
         LoginPage loginPage = new LoginPage();
     }
